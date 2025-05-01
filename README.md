d263ba88a32911e48eeb345674adb2c032ed67c2
Akanksha <akankshaanand2512@gmail.com>
2025-05-01 19:06:18 +0530

    making models

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a zoom clone.
+It is a video conferencing platform where people can connect through meeting code.
---
+++ backend/src/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
+import cors from "cors";
+import { log } from "node:console";
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", (process.env.port || 8000));
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+const start = async () => {
+    const connectionDB = await mongoose.connect(
+      "mongodb+srv://akankshaanand2512:micro2512@cluster0.uumckh1.mongodb.net/"
+    );
+    console.log(`Mongo connected, DB Host: ${connectionDB.connection.host}`);
+    
+    server.listen(app.get("port"), () => {
+        console.log("Listening on port 8000");
+    });
+}
+start();
